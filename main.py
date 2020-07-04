3f5fc5a9106bdf6275c70a213d70cc83fc55bd49
Garykun98 <rvalers98@gmail.com>
2020-07-04 07:35:08 -0500

    backend avance

--- main.py
+++ main.py
@@ -1,75 +1,74 @@
+#init.py
+##librerias
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
 
+
+##Inicializacion
 app = Flask(__name__)
-
-# Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = 'mysecretkey'
 
-# Enter your database connection details below
+##Configuracion MySQL (DB)
 app.config['MYSQL_HOST'] = 'us-cdbr-east-02.cleardb.com'
 app.config['MYSQL_USER'] = 'b629ab6019ca38'
 app.config['MYSQL_PASSWORD'] = '6ba7734f'
 app.config['MYSQL_DB'] = 'heroku_b7bee2d28031202'
 
-# Intialize MySQL
+##Inicializacion de MySQL
 mysql = MySQL(app)
 
+##Rutas
+###Index
 
-@app.route('/', methods=['GET', 'POST'])
+###Login
+@app.route('/', methods=['GET', 'POST']) #(index.html)
 def login():
-# Output message if something goes wrong...
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
+    if request.method == 'POST' and ('username' in request.form and 'password' in request.form):
         username = request.form['username']
         password = request.form['password']
-        # Check if account exists using MySQL
+        # Validacion de los datos
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
-        # Fetch one record and return result
+        global account 
         account = cursor.fetchone()
-                # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id_user'] = account['id_user']
-            session['username'] = account['username']
-            # Redirect to home page
-            return redirect(url_for('home'))
+        cursor.close()
+        if account == None:
+            msg = 'Username o password incorrectos'     
         else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    return render_template('index.html', msg='')
+            session['username'] = account['username']
+            print (session['username'])
+            return redirect(url_for('home'))
+    return render_template('index.html', msg = msg)
 
-
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST']) #(register.html)
 def register():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-                # Check if account exists using MySQL
+        # Agregar datos 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('INSERT INTO accounts(username, password, email) VALUES ( %s, %s, %s)', (username, password, email))
         mysql.connection.commit()
-        msg = 'You have successfully registered!'
-    # Show registration form with message (if any)
-    return render_template('register.html', msg=msg)
+        msg = 'Registro completo'
+    return render_template('register.html', msg = msg)
 
-# http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
+###Home
 @app.route('/home')
 def home():
-    return render_template('home.html')   
+    return render_template('home.html', data = account)
 
+@app.route('/profile')
+def profile():
+    return render_template('procfile.html', data = account)
 
-if __name__ =='__main__':
-	app.run(port=3000, debug=True)
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('index.html')
+##run
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
